545ed1f5bd536006c0f06405a61facbdbbb0e2f7
karira <hackster.origin@gmail.com>
2022-06-18 10:58:55 +0300

    added data.js

---
+++ data.js
@@ -0,0 +1,14 @@
+const studentData = [{"id":1,"name":"Abbe","course":"React","grade":"C","city":"Nairobi"},
+{"id":2,"name":"Biddy","course":"Python","grade":"D","city":"Maputo"},
+{"id":3,"name":"Avrit","course":"Java","grade":"B","city":"New York"},
+{"id":4,"name":"Darla","course":"JS","grade":"A","city":"Mumbai"},
+{"id":5,"name":"Shoshana","course":"Java","grade":"B","city":"London"},
+{"id":6,"name":"Elwood","course":"C#","grade":"C","city":"Nairobi"},
+{"id":7,"name":"Svend","course":"React","grade":"C","city":"Kigali"},
+{"id":8,"name":"Carleen","course":"Next","grade":"A","city":"Kigali"},
+{"id":9,"name":"Pat","course":"Java","grade":"D","city":"Johannesburg"},
+{"id":10,"name":"Arvy","course":"Python","grade":"E","city":"Kigali"},
+{"id":11,"name":"Charles","course":"Node","grade":"A","city":"Nairobi"},
+{"id":12,"name":"Mercy","course":"Figma","grade":"A","city":"Nairobi"}]
+
+module.exports = {studentData}
