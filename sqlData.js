4e528e3ebabf7dcb60a683c16fe889872c73f50a
karira <hackster.origin@gmail.com>
2022-06-22 09:59:53 +0300

    added sql commands

---
+++ sqlData.js
@@ -0,0 +1,15 @@
+let persons = [
+    {
+        name: "jack", age: "34"
+    },
+    {
+        name: "paul", age: "23"
+    },
+    {
+        name: "mercy", age: "24"
+    },{
+        name: "chelsea", age: "25"
+    }
+]
+
+module.exports = {persons}
